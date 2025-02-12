0ac2f60a02530868fc815e80882a7372dce8bd3b
Abdiel0205 <saulabdiel.garciacortina@unidemat.edu.mx>
2025-02-12 17:11:15 -0600

    csharp
    practica #3 de perimetro y area

--- Form1.cs
+++ Form1.cs
@@ -1,61 +1,50 @@
-namespace PRACTICA02
+using static System.Windows.Forms.MonthCalendar;
+
+namespace practica3
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            float cent = float.Parse(this.textBox1.Text);
-            float fair = (cent * 9 / 5) + 32;
-            this.Convertir.Text = fair.ToString();
-
-        }
-
-        private void lblIngrese_Click(object sender, EventArgs e)
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
+            if (Double.TryParse(TextBox1.Text, out double radio))
+            {
+                double area = Math.PI * Math.Pow(radio, 2);
+                Calcular1.Text = area.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("ingrese un valor válido para el radio.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void btnconvertir_Click(object sender, EventArgs e)
-        {
-            float cent = float.Parse(this.textBox2.Text);
-            float fair = (cent - 32) * 5 / 9;
-            this.Convertir1.Text = fair.ToString();
-        }
-
-        private void txtNum1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNum2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNum4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblingresefah_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Calcular2_Click(object sender, EventArgs e)
+        {
+            if (Double.TryParse(TextBox1.Text, out double radio))
+            {
+                double perimetro = 2 * Math.PI * radio;
+                Calcular2.Text = perimetro.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("ingrese un valor válido para el radio.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
