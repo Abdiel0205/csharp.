aae5360631422816c0f63560b3a68a17246b37b0
Abdiel0205 <saulabdiel.garciacortina@unidemat.edu.mx>
2025-02-19 16:18:30 -0600

    C#
    SACAR PROMEDIO DE ALUMNOS AVTIVIDAD 5

--- Form1.cs
+++ Form1.cs
@@ -1,67 +1,78 @@
-namespace Actividad2
+namespace practica05
 {
     public partial class Form1 : Form
     {
-        int vIndice = -1;
-        string vSeleccion;
+
         public Form1()
+
         {
             InitializeComponent();
-            comboBox1.Items.Add("Mexico");
-            comboBox1.Items.Add("USA");
-            comboBox1.Items.Add("Canada");
-            comboBox1.Items.Add("China");
-               
         }
 
-        private void btnADD_Click(object sender, EventArgs e)
-        {
-            vSeleccion = Convert.ToString(comboBox1.Text);
-            int vIndice = dataGridView1.Rows.Add();
-
-            dataGridView1.Rows[vIndice].Cells[0].Value = txtNAME.Text;
-            dataGridView1.Rows[vIndice].Cells[1].Value = txtLASTNAME.Text;
-            dataGridView1.Rows[vIndice].Cells[2].Value = vSeleccion;
-
-            txtNAME.Text = "";
-            txtLASTNAME.Text = "";
-            comboBox1.Text = "";
-        }
-
-        private void txtNAME_TextChanged(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
-        }
+            // Obtener las calificaciones ingresadas
+            double Parcial1 = Convert.ToDouble(txtParcial1.Text);
+            double Parcial2 = Convert.ToDouble(txtParcial2.Text);
+            double Parcial3 = Convert.ToDouble(txtParcial3.Text);
+            double Promedio = (Parcial1 + Parcial2 + Parcial3) / 3;
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            vIndice = e.RowIndex;
-        }
-
-        private void btnREMOVE_Click(object sender, EventArgs e)
-        {
-            if(vIndice != -1)
+            // Determinar el estatus del alumno
+            string Estatus;
+            if (Promedio >= 95)
             {
-                dataGridView1.Rows.RemoveAt(vIndice);
+                Estatus = "Exento";
             }
-        
+            else if (Promedio >= 70)
+            {
+                Estatus = "Ordinario";
+            }
+            else if (Promedio >= 50)
+            {
+                Estatus = "Extraordinario";
+            }
             else
             {
-                MessageBox.Show("Select a ITEM to delete");
-
+                Estatus = "Especial";
             }
+            int vIndice = dataGridView1.Rows.Add();
+            dataGridView1.Rows[vIndice].Cells[0].Value = txtNombre.Text;
+            dataGridView1.Rows[vIndice].Cells[1].Value = txtParcial1.Text;
+            dataGridView1.Rows[vIndice].Cells[2].Value = txtParcial2.Text;
+            dataGridView1.Rows[vIndice].Cells[3].Value = txtParcial3.Text;
+            dataGridView1.Rows[vIndice].Cells[4].Value = Promedio;
+            dataGridView1.Rows[vIndice].Cells[5].Value = Estatus;
+
+            txtNombre.Text = "";
+            txtParcial1.Text = "";
+            txtParcial2.Text = "";
+            txtParcial3.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
-
-
